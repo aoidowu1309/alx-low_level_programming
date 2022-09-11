d70059bac7bf0fdf5860f88a375b9cd8e73f6067
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-11 16:53:18 -0700

    2 Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,21 +6,18 @@
  * Return: 0
  */
 int main(void)
-{
 	int i;
 	int j;
-	for (i = 48; i < 57; i++)
-	{
-	for (j = i + 1; j <= 57; j++)
-		{
-		putchar(i);
-		putchar(j);
-		if (j == 57 && i == 56)
+	for ((i = 48; i < 57; i++)
+			{for (j = i + 1; j <= 57; j++)
+			{putchar(i);
+			putchar(j);
+			if (j == 57 && i == 56)
 			break;
 			putchar(44);
 			putchar(32);
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+			}
+			}
+			putchar('\n');
+			return (0);
+			}
