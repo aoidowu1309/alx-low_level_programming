ef5fedc54388e79abe75a0dd0abc32adc36024f9
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-12 06:10:36 -0700

    Print lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-/*
+/**
  *main - Start the coding here my friend
  *Description: prints  the alphabet in lowercase
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-	/* code goes here
-	 */
 	char ball;
 	for (ball = 'a'; ball <= 'z'; ball++)
 
-		putchar(ball);
+	putchar(ball);
 	putchar('\n');
 	return (0);
 }
