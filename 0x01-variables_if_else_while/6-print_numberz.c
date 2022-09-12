2c85cad316fa60a2bfccc73147bf1e7675003199
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-12 02:04:01 -0700

    Print Number 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *
- * * main - about to script another dope code
- *
- * * Description - using the main function
- *
- * * this program prints all single digit numbers of base 10 starting from 0
- *
- * * Return: 0
- *
+ *main - about to script another dope code
+ * Description - using the main function
+ * this program prints all single digit numbers of base 10 starting from 0
+ * Return: 0
  */
 int main(void)
 {
