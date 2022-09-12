9167f4f7c911f3065e40b0321ec853a66e6ab98d
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-12 05:43:00 -0700

    Print Comb 5

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,14 @@
-#include <stdio.h>
-/**
- * main - ending this in style
- * Description: using the main function
- * this program prints all possible combinations of two two-digit numbers
- * Return: 0
+#include<stdio.h>
+/*
+ * main - Start the coding here
+ * Description: print the alphabet in lower case
+ * Return: Always 0 (success)
  */
 int main(void)
+	int ch;
+	clrscr();
+	for (ch= 'a'; ch<= 'Z'; ch++)
+	putchar(ch);
+	putchar('n\');
+	getch();
 {
-	int i, o;
-	for (i = 0; i < 100; i++)
-	{
-		for (o = i + 1; o < 100; o++)
-		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((o/10) + '0');
-			putchar((o%10) + '0');
-			if (i == 98 && o == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
-}
