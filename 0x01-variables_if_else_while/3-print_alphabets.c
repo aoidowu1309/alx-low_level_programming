8b5831f35e1a8b130ace5772187fa74e7b664bfb
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-12 01:31:35 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 /**
- * main - let's keep coding
+ * main - Start the coding boy
  * Description: using the main function
- * this program prints all possible different combinations of three digits
+ * this program prints the alphabet in lowercase, and then in uppercase
  * Return: 0
  */
 int main(void)
 {
-	int i, o, u;
-	for (i = '0'; i <= '9'; i++)
-{
-	for (o = i + 1; o <= '9'; o++)
-{
-	for (u = o + 1; u <= '9'; u++)
-{
-	if ((o != i) && (u != o))
-{
-	putchar(i);
-	putchar(o);
-	putchar(u);
-	 if (i == '7' && o == '8')
-	 continue;
-	putchar(',');
-	putchar(' ');
-}
-}
-}
-}
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
