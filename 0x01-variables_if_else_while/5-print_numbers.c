dca76d6f580d74aeacffde513cc3c5723d0a0dac
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-11 17:27:56 -0700

    Lets print 5 Alphabets

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,18 +2,14 @@
 /**
  *
  * main - another code goes here
- *
  * Description: using the main function
- *
  * this program prints all single digit numbers of base 10 starting from 0
- *
  * Return: 0
- *
  */
 int main(void)
 {
 	int i;
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++);
 	{
 		printf("%i", i);
 	}
