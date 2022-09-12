9d3745950247e3e564e9ab6216f28f5400c056c9
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-12 01:46:19 -0700

    Print Alphabet 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
-
 /**
- *
- * main - ending this in style
- *
+ * main - kindly know that this code works
  * Description: using the main function
- *
- * this program prints all possible combinations of two two-digit numbers
- *
+ * this program prints the alphabet in lowercase
  * Return: 0
- *
  */
 int main(void)
 {
-	int i, o;
-	for (i = 0; i < 100; i++)
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		for (o = i + 1; o < 100; o++)
+		if (ch != 'e' && ch != 'q')
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((o / 10) + '0');
-			putchar((o % 10) + '0');
-			if (i == 98 && o == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			putchar(ch);
 		}
 	}
 	putchar('\n');
