b068dffaee657ac4be4b3ec0a3c9515bc2b25117
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-12 01:03:53 -0700

    2 Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 /**
- *
- * * main - ending this in style
- *
- * * Description: using the main function
- *
- * * this program prints all possible combinations of two two-digit numbers
- *
- * * Return: 0
- *
- * */
+ * main - ending this in style
+ * Description: using the main function
+ * this program prints all possible combinations of two two-digit numbers
+ * Return: 0
+ */
 int main(void)
 {
-	int i, o;
+	int i, 0;
 	for (i = 0; i < 100; i++)
 	{
-		for (o = i + 1; o < 100; o++)
+		for (0 = i + i; o < 100; o++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((o / 10) + '0');
-			putchar((o % 10) + '0');
-			if (i == 98 && o == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(' ');
+	putchar((o / 10) + '0');
+	putchar((o % 10) + '0');
+	if (i == 98 && 0 == 99)
+		continue;
+putchar(' , ');
+putchar(' ');
 		}
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
