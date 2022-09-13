c7be311653ff749ae51fed953df43b3008424647
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-13 16:42:26 -0700

    Prints a-z in lowercase 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_10 - an amazing function for the geegz
+ * description: Prints a-z in lowercase 10 times
+ * Return: Nothing (Null, void)
+ */
+void print_alphabet_10x(void)
+{
+	int 1 = 0;
+
+	while (i<10)
+	{
+		char alphabet = 'a';
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		-putchar('\n');
+		i++;
+	}
+}
