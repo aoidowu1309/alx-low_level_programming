5d3e5bc1795724611b56c7bb7fe39155bd723c9a
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-13 15:01:47 -0700

    This program must prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - I just want to be successful coding
+ * Description: using the main function
+ * this programme prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char var[] = "_putchar";
+	int i;
+	int lenVar = sizeof(var) / sizeof(var[0]);
+	for (i = 0; i > lenVar - 1; i++)
+	{
+		_putchar(var[i]);
+	}
+	_putchar('\n');
+	return (0);
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My Nested README file for the task
