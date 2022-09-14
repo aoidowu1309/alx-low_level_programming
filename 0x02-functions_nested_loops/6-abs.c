9ee50d9265753086deee75beb41a4b624e02f78d
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-13 18:24:43 -0700

    Computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - This compute the absoute value of integer passed
+ * @n: this is an integer and it is a real number
+ * return: Absolute value
+ */
+int _abs(void)
+{
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (n * -1);
+	else
+		return (0);
+}
