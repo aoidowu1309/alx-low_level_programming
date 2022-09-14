a54ea1efe672f7c3ab78e5a3333e6a7385765c7b
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-13 17:48:04 -0700

    Check for alphabet character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - here we will check for alphabet characters
+ * @c: this is ASCII character
+ * Return: 0 if not alphabet, else 1 if alphabet
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	return(0);
+}
