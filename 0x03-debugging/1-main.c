81b561524b1f298a7753ffc6005a1589af10e24f
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-15 02:50:51 -0700

    Please Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,13 +9,13 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-	/*
-	 *while (i < 10)
+	/**
+	 * while (i < 10)
 	 *{
-	 *Infinite loop - No increase of variable
-	 *putchar(i);
+	 * Infinite loop
+	 * putchar(i);
 	 *}
-	 */	
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
