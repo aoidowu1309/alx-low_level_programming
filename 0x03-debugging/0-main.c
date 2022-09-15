234dbe683ee7ebc71312c7ea7ad3c58b29d628cd
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-15 01:55:29 -0700

    Debugging the threat

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - lets run a test function
+ * main - lets run a test function that is capable of printing +ve or -ve integers
  * Return: 0
  */
 int main(void)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
