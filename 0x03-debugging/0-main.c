e75f80ee0cdb4ab2c7f16100e3ae1b873680ea94
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-15 10:26:50 -0700

    Debugging the threat

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * main - lets run a test function that is capable 
- * of printing positive or negative integers
+ * main - main
  * Return: 0
  */
 int main(void)
-{	
+{
 	int i;
 
 	i = 0;
