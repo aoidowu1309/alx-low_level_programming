c2d53a3a3d61c639862619e343a4151e84f5b1dc
AJAYI IDOWU <aoidowu1309@gmail.com>
2022-09-21 15:09:16 -0700

    Let this function print buffer

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,14 +8,15 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of code here...
-	 * Note:
+	 * Write your line of code here...
+	 * Remember:
 	 * You are not allowed to use a
 	 * You are not allowed to modify p
-	 * Only one statement is allowed
+	 * Only one statement
 	 * You are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */  printf("a[2] = %d\n", a[2]);
-	  return (0);
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
